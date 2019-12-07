b9c1d677465f32520e3fb814caba2db7f886e764
Еvgenij Pushkaryov <rosrys2013@mail.ru>
2019-12-07 09:56:55 +0300

    page styling

--- src/pages/Prom_tara/Prom_tara.js
+++ src/pages/Prom_tara/Prom_tara.js
@@ -1,1 +1,21 @@
 import './Prom_tara.scss'
+import './owlcarousel/owl.carousel.css'
+import './owlcarousel/owl.theme.default.css'
+import './owlcarousel/owl.carousel.min'
+import './owlcarousel/animate.css'
+import './owlcarousel/owl.theme.green.css'
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin: 2,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        navigation : true,
+        singleItem : true,
+        animateIn: 'fadeIn',
+        nav: true,
+        navText: [],
+    });
+});             
